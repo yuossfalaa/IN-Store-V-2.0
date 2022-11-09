6650177d1e58637f3dfc1b32fb1d95c5b300501c
yuossfalaa <yuossfalla@gmail.com>
2022-11-09 11:31:16 +0200

    Update FrameWork To .NET 7

--- INStore/App.xaml.cs
+++ INStore/App.xaml.cs
@@ -13,5 +13,11 @@ namespace INStore
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.Show();
+            base.OnStartup(e);
+        }
     }
 }
