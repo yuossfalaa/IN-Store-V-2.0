5d117a1253ad57c9a4f7abd4f56967424935efdd
yuossfalaa <yuossfalla@gmail.com>
2022-11-14 18:27:30 +0200

    Navigator

--- INStore/State/Navigators/Navigator.cs
+++ INStore/State/Navigators/Navigator.cs
@@ -1,13 +1,29 @@
 ﻿using INStore.Commands;
 using INStore.ViewModels;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace INStore.State.Navigators
 {
-    public class Navigator : INavigator
+    public class Navigator : INavigator , INotifyPropertyChanged
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set 
+            { 
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
 
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));    
+        }
     }
 }
