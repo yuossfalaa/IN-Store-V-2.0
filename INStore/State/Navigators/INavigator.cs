a1980873db6dd9cea491bbf24d055a52d17d5c47
yuossfalaa <yuossfalla@gmail.com>
2022-11-14 18:06:02 +0200

    Adding Navigator

---
+++ INStore/State/Navigators/INavigator.cs
@@ -0,0 +1,26 @@
+﻿using INStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INStore.State.Navigators
+{
+    public interface INavigator
+    {
+        public enum ViewType
+        {
+            Home,
+            About,
+            DashBoard,
+            MyStore,
+            RefundDashboard,
+            SellerDashboard,
+            Tools
+        }
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
---
+++ INStore/State/Navigators/Navigator.cs
@@ -0,0 +1,13 @@
+﻿using INStore.Commands;
+using INStore.ViewModels;
+using System.Windows.Input;
+
+namespace INStore.State.Navigators
+{
+    public class Navigator : INavigator
+    {
+        public ViewModelBase CurrentViewModel { get; set; }
+
+        public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+    }
+}
