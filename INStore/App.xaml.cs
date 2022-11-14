2c90fdd595af089e8ce5f89ca3deabd70e8366be
yuossfalaa <yuossfalla@gmail.com>
2022-11-14 17:46:27 +0200

    Adding View models

--- INStore/App.xaml.cs
+++ INStore/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace INStore
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
