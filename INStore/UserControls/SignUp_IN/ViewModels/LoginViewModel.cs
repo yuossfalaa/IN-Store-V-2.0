340fce29d3e1c4a295424fee053f60d0b987f6f3
yuossfalaa <yuossfalla@gmail.com>
2023-01-29 04:05:08 +0200

    Add NEW V/Vm

---
+++ INStore/UserControls/SignUp_IN/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using INStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.UserControls.SignUp_IN.ViewModels
+{
+    class LoginViewModel : ViewModelBase
+    {
+    }
+}
