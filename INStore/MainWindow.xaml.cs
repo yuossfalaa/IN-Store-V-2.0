369926b961278ee02c30e99481c83f2f0b181e41
yuossfalaa <yuossfalla@gmail.com>
2023-01-29 03:34:48 +0200

    New Adds

--- INStore/MainWindow.xaml.cs
+++ INStore/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -22,6 +23,64 @@ namespace INStore
         public MainWindow()
         {
             InitializeComponent();
+            size();
+        }
+        public void size()
+        {
+
+            int width = (int)System.Windows.SystemParameters.PrimaryScreenWidth;
+            int hight = (int)System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.MaxHeight = System.Windows.SystemParameters.MaximizedPrimaryScreenHeight;
+
+            if (width <= 1280 || hight <= 690)
+            {
+                this.Width = System.Windows.SystemParameters.MaximizedPrimaryScreenWidth;
+                this.Height = System.Windows.SystemParameters.MaximizedPrimaryScreenHeight;
+            }
+        }
+
+        private void CloseApplication(object sender, RoutedEventArgs e)
+        {
+            var anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(0.1));
+            anim.Completed += (s, _) => this.Close();
+            this.BeginAnimation(UIElement.OpacityProperty, anim);
+        }
+
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+
+                this.WindowState = WindowState.Maximized;
+                MaximizeWindowPackIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                MaximizeWindowPackIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.FullscreenExit;
+            }
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TriggerMoveWindow(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (WindowState == System.Windows.WindowState.Maximized)
+                {
+                    WindowState = System.Windows.WindowState.Normal;
+
+                    double pct = PointToScreen(e.GetPosition(this)).X / System.Windows.SystemParameters.PrimaryScreenWidth;
+                    Top = 0;
+                    Left = e.GetPosition(this).X - (pct * Width);
+                }
+
+                DragMove();
+            }
         }
     }
 }
