becac92b8615a9277a8c7cef7a0660e2d1dc1220
yuossfalaa <yuossfalla@gmail.com>
2023-01-30 12:58:16 +0200

    .

--- INStore/App.xaml.cs
+++ INStore/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using INStore.EntityFramework;
+using INStore.State.Navigators;
 using INStore.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -8,22 +13,68 @@ namespace INStore
 {
     public partial class App : Application
     {
-        private readonly INStoreDbContextFactory iNStoreDbContextFactory = new INStoreDbContextFactory();
-        protected override void OnStartup(StartupEventArgs e)
+        private readonly IHost _host;
+        public App()
         {
-            DbContextCreator();
-            Window window = new MainWindow();
-            window.DataContext = new MainViewModel();
-            window.Show();
-            base.OnStartup(e);
+           _host = Host.CreateDefaultBuilder()
+                 .UseSerilog((host, loggerConfiguration) =>
+                 {
+                     loggerConfiguration.WriteTo.File(LoggingPathAssembler().Result, rollingInterval: RollingInterval.Day)
+                         .WriteTo.Debug()
+                         .MinimumLevel.Information();
+                 })
+                 .ConfigureServices(services =>
+                 {
+                     services.AddScoped<MainViewModel>();
+                     services.AddScoped<INavigator,Navigator> ();
+
+                     services.AddSingleton<INStoreDbContextFactory>();
+                     services.AddScoped<MainWindow>(s => new MainWindow(_host!.Services.GetRequiredService<MainViewModel>()));
+
+                 })
+                 .Build(); 
         }
-        async Task DbContextCreator()
+        private async Task DbContextCreator()
         {
-            using (INStoreDbContext iNStoreDbContext = iNStoreDbContextFactory.CreateDbContext())
+            using (INStoreDbContext iNStoreDbContext = _host.Services.GetRequiredService<INStoreDbContextFactory>().CreateDbContext())
             {
                 iNStoreDbContext.Database.MigrateAsync().Wait();
             }
-            
+
+        }
+        private async Task<string> LoggingPathAssembler()
+        {
+            string LoggingPath = "";
+            try
+            {
+                LoggingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); //AppData\Roaming
+                LoggingPath = LoggingPath + @"\IN Store\Logs";
+                System.IO.Directory.CreateDirectory(LoggingPath);
+                LoggingPath += @"\INStore - .txt";
+
+            }
+            catch (Exception ConnectionStringAssemblerFailed)
+            {
+
+            }
+            return LoggingPath;
+
+
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            DbContextCreator();
+            await _host.StartAsync();
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
         }
     }
 }
