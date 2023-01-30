450efd760bd2b64a5327bbbef7d0f4fe76dbc6b8
yuossfalaa <yuossfalla@gmail.com>
2023-01-30 12:58:11 +0200

    .

--- INStore/ViewModels/MainViewModel.cs
+++ INStore/ViewModels/MainViewModel.cs
@@ -1,12 +1,19 @@
-﻿using INStore.UserControls.SignUp_IN.ViewModels;
+﻿using INStore.State.Navigators;
+using INStore.UserControls.SignUp_IN.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace INStore.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
 
-        public MainViewModel()
+        private readonly ILogger<MainViewModel> _MainViewModelLogger;
+        public INavigator Navigator { get; set; }
+
+        public MainViewModel(ILogger<MainViewModel> MainViewModelLogger , INavigator navigator)
         {
+            _MainViewModelLogger = MainViewModelLogger;
+            Navigator = navigator;
             Navigator.CurrentViewModel = new LoginViewModel();
         }
     }
--- INStore/ViewModels/ViewModelBase.cs
+++ INStore/ViewModels/ViewModelBase.cs
@@ -1,11 +1,20 @@
 ﻿using INStore.State.Navigators;
+using System.ComponentModel;
 
 namespace INStore.ViewModels
 {
 
-    public class ViewModelBase
-    {
-        public INavigator Navigator { get; set; } = new Navigator();
 
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public virtual void Dispose() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+
 }
