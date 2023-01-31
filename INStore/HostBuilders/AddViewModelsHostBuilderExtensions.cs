3c1c564370e55963e3a295a65d893ab71a5d5f08
yuossfalaa <yuossfalla@gmail.com>
2023-01-31 09:36:09 +0200

    Add View Models Host Builder

---
+++ INStore/HostBuilders/AddViewModelsHostBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using INStore.Factories;
+using INStore.State.Navigators;
+using INStore.UserControls.Home.ViewModels;
+using INStore.UserControls.SignUp_IN.ViewModels;
+using INStore.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+
+namespace INStore.HostBuilders
+{
+    public static class AddViewModelsHostBuilderExtensions
+    {
+        public static IHostBuilder AddViewModels(this IHostBuilder host)
+        {
+            host.ConfigureServices(services => 
+            {
+                services.AddTransient(CreateHomeViewModel);
+                services.AddTransient<MainViewModel>();
+
+                services.AddSingleton<CreateViewModel<HomeViewModel>>(services => () => services.GetRequiredService<HomeViewModel>());
+                services.AddSingleton<CreateViewModel<LoginViewModel>>(services => () => CreateLoginViewModel(services));
+
+                services.AddSingleton<IINStoreViewModelFactory, INStoreViewModelFactory>();
+                services.AddSingleton<ViewModelDelegateRenavigator<HomeViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<LoginViewModel>>();
+            });
+            return host;
+        }
+        private static HomeViewModel CreateHomeViewModel(IServiceProvider services)
+        {
+            return new HomeViewModel();
+        }
+
+        private static LoginViewModel CreateLoginViewModel(IServiceProvider services)
+        {
+            return new LoginViewModel();
+        }
+    }
+
+}
