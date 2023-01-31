64157bd9515e91cab0a9e3373ed577b77115888d
yuossfalaa <yuossfalla@gmail.com>
2023-01-31 09:36:33 +0200

    .

---
+++ INStore/State/Navigators/ViewModelDelegateRenavigator.cs
@@ -0,0 +1,28 @@
+﻿using INStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.State.Navigators
+{
+
+    public class ViewModelDelegateRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public ViewModelDelegateRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+
+}
