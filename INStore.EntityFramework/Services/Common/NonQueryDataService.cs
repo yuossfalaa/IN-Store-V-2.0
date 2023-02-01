cee630a50babe0974c5ad707b7f42e036f25ffb1
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 07:34:12 +0200

    adding NQDS

---
+++ INStore.EntityFramework/Services/Common/NonQueryDataService.cs
@@ -0,0 +1,52 @@
+﻿using INStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace INStore.EntityFramework.Services.Common
+{
+    public class NonQueryDataService<T> where T : DomainObject
+    {
+        private readonly INStoreDbContextFactory _contextFactory;
+
+        public NonQueryDataService(INStoreDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+        public async Task<T> Create(T entity)
+        {
+            using (INStoreDbContext context = _contextFactory.CreateDbContext())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            using (INStoreDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+
+        public async Task<bool> Delete(T Entity)
+        {
+            using (INStoreDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == Entity.Id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+    }
+}
