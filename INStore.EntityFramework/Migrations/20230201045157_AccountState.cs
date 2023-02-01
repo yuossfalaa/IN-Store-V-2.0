4ceee9404e54c4bbcfcf96967b0ff0ac50ddf8af
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 07:11:47 +0200

    .

---
+++ INStore.EntityFramework/Migrations/20230201045157_AccountState.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INStore.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountState",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "AccountState",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
