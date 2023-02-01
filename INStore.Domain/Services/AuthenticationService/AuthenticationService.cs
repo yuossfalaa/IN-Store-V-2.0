aa2754482033881a9dc228d3590a5558c0a64775
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 07:11:40 +0200

    .

---
+++ INStore.Domain/Services/AuthenticationService/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using INStore.Domain.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.Domain.Services.AuthenticationService
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public async Task<User> LogIn(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Registre(string UserName, string Password, string AdminPassword, string AccountState, string EmployeeName)
+        {
+            IPasswordHasher hasher = new PasswordHasher();
+            string HashedPassword = hasher.HashPassword(Password);
+            string HashedAdminPassword = hasher.HashPassword(AdminPassword);
+
+            if (AdminPassword == AdminPassword)    
+            {
+                User user = new User()
+                {
+                    UserName = UserName,
+                    PasswordHash = HashedPassword,
+                    AdminPasswordHash = HashedAdminPassword,
+                    AccountState = AccountState,
+                    EmployeeName = EmployeeName
+                };
+                return true;
+            }
+            return false;
+        }
+
+
+
+        
+        }
+    }
---
+++ INStore.Domain/Services/AuthenticationService/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using INStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.Domain.Services.AuthenticationService
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Registre(string UserName,
+            string Password, string AdminPassword,
+            string AccountState, string EmployeeName);
+
+        Task<User> LogIn(string UserName, string Password);
+    }
+}
