33a96ac92077e2ddf22d0a17a151a825431e96ac
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 10:41:58 +0200

    .

--- INStore.Domain/Services/AuthenticationService/AuthenticationService.cs
+++ INStore.Domain/Services/AuthenticationService/AuthenticationService.cs
@@ -1,27 +1,65 @@
-﻿using INStore.Domain.Models;
+﻿using INStore.Domain.Exceptions;
+using INStore.Domain.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static INStore.Domain.Services.AuthenticationService.IAuthenticationService;
 
 namespace INStore.Domain.Services.AuthenticationService
 {
     public class AuthenticationService : IAuthenticationService
     {
-        public async Task<User> LogIn(string UserName, string Password)
+        private readonly  IPasswordHasher _passwordhasher;
+        private readonly  IUserService _userService;
+
+        public AuthenticationService(IUserService userService, IPasswordHasher passwordhasher)
         {
-            throw new NotImplementedException();
+            _userService = userService;
+            _passwordhasher = passwordhasher;
         }
 
-        public async Task<bool> Registre(string UserName, string Password, string AdminPassword, string AccountState, string EmployeeName)
+        public async Task<User> LogIn(string UserName, string Password)
         {
-            IPasswordHasher hasher = new PasswordHasher();
-            string HashedPassword = hasher.HashPassword(Password);
-            string HashedAdminPassword = hasher.HashPassword(AdminPassword);
+            User StoredUser = await _userService.GetByUsername(UserName);
+            if (StoredUser == null) 
+            {
+                throw new UserNotFoundException(UserName); 
+            }
+            PasswordVerificationResult passwordResult = _passwordhasher.VerifyHashedPassword(StoredUser.PasswordHash, Password);
+            if (passwordResult != PasswordVerificationResult.Success)
+            {
+                throw new UserNotFoundException(UserName,Password);
+            }
+            return StoredUser;
+        }
 
-            if (AdminPassword == AdminPassword)    
+        public async Task<RegistrationResult> Registre(string UserName, string Password, string AdminPassword, string AccountState, string EmployeeName)
+        {
+            RegistrationResult result = RegistrationResult.Success;
+            string HashedPassword = _passwordhasher.HashPassword(Password);
+            string HashedAdminPassword = _passwordhasher.HashPassword(AdminPassword);
+
+
+            User UserNameCheck = await _userService.GetByUsername(UserName);
+            if (UserNameCheck != null) 
+            {
+                result = RegistrationResult.UsernameAlreadyExists;
+            }
+
+            User AdminPasswordCheck = await _userService.GetByUsername(HashedAdminPassword);
+            if (AdminPassword != "excELI")
+            {
+                if (AdminPasswordCheck == null)
+                {
+                    result = RegistrationResult.AdminPasswordDoNotMatch;
+                }
+
+            }
+
+            if (result == RegistrationResult.Success)    
             {
                 User user = new User()
                 {
@@ -31,13 +69,10 @@ namespace INStore.Domain.Services.AuthenticationService
                     AccountState = AccountState,
                     EmployeeName = EmployeeName
                 };
-                return true;
+                await _userService.Create(user);
+                
             }
-            return false;
+            return result;
         }
-
-
-
-        
         }
     }
