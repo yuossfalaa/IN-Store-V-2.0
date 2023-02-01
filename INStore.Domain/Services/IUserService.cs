cc9643225e67efa043d561e6e9a31f9556294c3f
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 07:34:00 +0200

    adding IUser Service

---
+++ INStore.Domain/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using INStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.Domain.Services
+{
+    public interface IUserService : IDataService<User>
+    {
+        Task<User> GetByUsername(string username);
+    }
+}
