211f4f2a2ec146babdfd8e0fa791342b6a332621
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 06:45:40 +0200

    adding Password Hash

--- INStore/UserControls/SignUp_IN/ViewModels/LoginViewModel.cs
+++ INStore/UserControls/SignUp_IN/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using INStore.ViewModels;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,21 @@ namespace INStore.UserControls.SignUp_IN.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        private readonly ILogger<LoginViewModel> _MainViewModelLogger;
+
+        public LoginViewModel(ILogger<LoginViewModel> mainViewModelLogger)
+        {
+            _MainViewModelLogger = mainViewModelLogger;
+            text = "brah";
+            mainViewModelLogger.LogError(text);
+        }
+        private string _text;
+
+        public string text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged(nameof(text)); }
+        }
+
     }
 }
