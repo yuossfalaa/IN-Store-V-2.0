8f5abe18e02d6c4671f734a741e4c5f58c94eb33
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 19:20:08 +0200

    adding Login Command

---
+++ INStore/UserControls/SignUp_IN/Commands/LoginCommand.cs
@@ -0,0 +1,24 @@
+﻿using INStore.Commands;
+using INStore.Domain.Services.AuthenticationService;
+using INStore.UserControls.SignUp_IN.ViewModels;
+using System.Threading.Tasks;
+
+namespace INStore.UserControls.SignUp_IN.Commands
+{
+    public class LoginCommand : AsyncCommandBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly LoginViewModel _loginViewModel;
+
+        public LoginCommand(LoginViewModel loginViewModel, IAuthenticationService authenticationService)
+        {
+            _loginViewModel = loginViewModel;
+            _authenticationService = authenticationService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            await _authenticationService.LogIn(_loginViewModel.Username, _loginViewModel.Password);
+        }
+    }
+}
