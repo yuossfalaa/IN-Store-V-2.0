6cbb077043649e90ae973def33759ce93ef0487d
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 18:23:30 +0200

    adding Localization

---
+++ INStore/Language/LocalizedStrings.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using WPFLocalizeExtension.Engine;
+
+namespace INStore.Language
+{
+    internal class LocalizedStrings
+    {
+        private LocalizedStrings()
+        {
+
+        }
+
+        public static LocalizedStrings Instance { get; } = new LocalizedStrings();
+
+        public void SetCulture(string cultureCode)
+        {
+            var newCulture = new CultureInfo(cultureCode);
+            LocalizeDictionary.Instance.Culture = newCulture;
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                var result = LocalizeDictionary.Instance.GetLocalizedObject("INStore", "Strings", key, LocalizeDictionary.Instance.Culture);
+                return result as string;
+            }
+        }
+    }
+}
