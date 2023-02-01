b15e347ecd90b119a98fe82fec8182e1045572e7
yuossfalaa <yuossfalla@gmail.com>
2023-02-01 10:41:44 +0200

    adding Excption

---
+++ INStore.Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace INStore.Domain.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string username)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string? message, string username) : base(message)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string? message, Exception? innerException, string username) : base(message, innerException)
+        {
+            Username = username;
+        }
+
+
+        public string Username { get; set; }
+  
+    }
+}
