36e344e62bbd8cea3ca8484a91f657f80a271b14
yuossfalaa <yuossfalla@gmail.com>
2023-02-02 12:04:26 +0200

    Adding USerControl

---
+++ INStore/State/UserStore/IUserStore.cs
@@ -0,0 +1,12 @@
+﻿using INStore.Domain.Models;
+using System;
+
+namespace INStore.State.UserStore
+{
+    public interface IUserStore
+    {
+        User CurrentUser { get; set; }
+
+        event Action StateChanged;
+    }
+}
---
+++ INStore/State/UserStore/UserStore.cs
@@ -0,0 +1,29 @@
+﻿using INStore.Domain.Models;
+using INStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStore.State.UserStore
+{
+    public class UserStore : IUserStore
+    {
+        private User _currentUser;
+
+        public User CurrentUser
+        {
+            get
+            {
+                return _currentUser;
+            }
+            set
+            {
+                _currentUser = value;
+                StateChanged?.Invoke();
+            }
+        }
+        public event Action StateChanged;
+    }
+}
