7cbb0b38ad9e3d0502b9466c8a956663492e1b36
yuossfalaa <yuossfalla@gmail.com>
2023-02-02 15:38:02 +0200

    Changeing User Domain

--- INStore.Domain/Models/User.cs
+++ INStore.Domain/Models/User.cs
@@ -1,13 +1,17 @@
 ﻿namespace INStore.Domain.Models
 {
+    public enum AccountState
+    {
+        Admin,
+        Employee
+    }
     public class User : DomainObject
     {
         public string? UserName { get; set; } 
         public string? PasswordHash { get; set; } 
         public string? AdminPasswordHash { get; set; } 
-        public string? AccountState { get; set; }
-        public string? EmployeeName { get; set; }
-    
+        public AccountState? AccountState { get; set; }
+        public Employee employee { get; set; }    
 
 
     }
