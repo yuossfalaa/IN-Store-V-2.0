01517c06a545bf77515360f7d43ba819772e2f74
yuossfalaa <yuossfalla@gmail.com>
2023-02-04 08:53:18 +0200

    adding RegisterViewModel

---
+++ INStore/UserControls/SignUp_IN/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using INStore.State.Authenticators;
+using INStore.State.Navigators;
+using INStore.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace INStore.UserControls.SignUp_IN.ViewModels
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        private readonly ILogger<RegisterViewModel> _RegisterViewModelLogger;
+        private readonly IAuthenticators _Authenticators;
+        private readonly IRenavigator _loginRenavigator;
+        public RegisterViewModel(
+            ILogger<RegisterViewModel> RegisterViewModelLogger, 
+            IAuthenticators authenticators,
+            IRenavigator loginRenavigator)
+        {
+            _RegisterViewModelLogger = RegisterViewModelLogger;
+            _Authenticators = authenticators;
+            _loginRenavigator = loginRenavigator;
+        }
+    }
+}
