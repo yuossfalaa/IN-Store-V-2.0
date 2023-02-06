fae1f0b099983ea7c72af7a416f1e9f63645a66b
yuossfalaa <yuossfalla@gmail.com>
2023-02-06 14:46:07 +0200

    Adding Logging

--- INStore/UserControls/SignUp_IN/Commands/RegisterCommand.cs
+++ INStore/UserControls/SignUp_IN/Commands/RegisterCommand.cs
@@ -2,6 +2,9 @@
 using INStore.State.Authenticators;
 using INStore.State.Navigators;
 using INStore.State.UserStore;
+using INStore.UserControls.SignUp_IN.ViewModels;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,23 +19,48 @@ namespace INStore.UserControls.SignUp_IN.Commands
         private readonly IAuthenticators authenticators;
         private readonly IInRegistrationUser inRegistrationUser;
         private readonly IRenavigator renavigator;
-
-        public RegisterCommand(IAuthenticators authenticators, IInRegistrationUser inRegistrationUser, IRenavigator renavigator)
+        private readonly ISnackbarMessageQueue snackbarMessageQueue;
+        ILogger<RegisterEmployeeViewModel> logger;
+        public RegisterCommand(IAuthenticators authenticators, IInRegistrationUser inRegistrationUser, 
+            IRenavigator renavigator, ISnackbarMessageQueue snackbarMessageQueue, 
+            ILogger<RegisterEmployeeViewModel> logger)
         {
             this.authenticators = authenticators;
             this.inRegistrationUser = inRegistrationUser;
             this.renavigator = renavigator;
+            this.snackbarMessageQueue = snackbarMessageQueue;
+            this.logger = logger;
         }
 
         public override async Task ExecuteAsync(object parameter)
         {
-            RegistrationResult result = RegistrationResult.Success;
-
-            result = await authenticators.Register(inRegistrationUser);
-            if (result == RegistrationResult.Success)
+            try
             {
-                renavigator.Renavigate();
+                RegistrationResult result = RegistrationResult.Success;
+
+                result = await authenticators.Register(inRegistrationUser);
+                if (result == RegistrationResult.Success)
+                {
+                    renavigator.Renavigate();
+                }
+                else if (result == RegistrationResult.AdminPasswordDoNotMatch)
+                {
+                    snackbarMessageQueue.Enqueue("Wrong Admin Password");
+                    logger.LogTrace("Wrong Admin Password");
+
+                }
+                else if (result == RegistrationResult.UsernameAlreadyExists)
+                {
+                    snackbarMessageQueue.Enqueue("Username Already Exists");
+                    logger.LogTrace("Username Already Exists");
+
+                }
             }
+            catch(Exception excption)
+            {
+                logger.LogError(excption.Message);
+            }
+     
         }
     }
 }
