e82b6b6775ac3ca4d9c23e9cacafe3425d4dda96
yuossfalaa <yuossfalla@gmail.com>
2023-02-06 17:59:07 +0200

    adding ILanguage

---
+++ INStore/Language/ILanguageSetter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace INStore.Language
+{
+    public interface ILanguageSetter
+    {
+        public bool IsArabic();
+        public bool IsEnglish();
+        public void SetToArabic();
+        public void SetToEnglish();
+        private void SetLanguage(CultureInfo Newculture) { }
+    }
+}
---
+++ INStore/Language/LanguageSetter.cs
@@ -0,0 +1,43 @@
+﻿using INStore.State.Registers;
+using System.Globalization;
+
+namespace INStore.Language
+{
+    public class LanguageSetter : ILanguageSetter
+    {
+        private readonly IRegister register;
+        private  CultureInfo cultureInfo;
+        public LanguageSetter(IRegister register)
+        {
+            this.register = register;
+            cultureInfo = register.GetLanguage();
+            WPFLocalizeExtension.Engine.LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+            WPFLocalizeExtension.Engine.LocalizeDictionary.Instance.Culture = cultureInfo;
+        }
+
+        private void SetLanguage(CultureInfo Newculture)
+        {
+            register.SetLanguage(Newculture);
+            cultureInfo = Newculture;
+            WPFLocalizeExtension.Engine.LocalizeDictionary.Instance.Culture = cultureInfo;
+        }
+        public bool IsArabic()
+        {
+            return cultureInfo.EnglishName == "Arabic (Egypt)";
+        }
+        public bool IsEnglish()
+        {
+            return cultureInfo.EnglishName == "English (United States)";
+        }
+
+        public void SetToArabic()
+        {
+            SetLanguage(new CultureInfo("en-US"));
+        }
+
+        public void SetToEnglish()
+        {
+            SetLanguage(new CultureInfo("ar-EG"));
+        }
+    }
+}
