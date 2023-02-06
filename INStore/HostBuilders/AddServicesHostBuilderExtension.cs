a7584e7236f44388b11628d45fd81d1635a5da55
yuossfalaa <yuossfalla@gmail.com>
2023-02-06 17:58:59 +0200

    adding ILanguage

--- INStore/HostBuilders/AddServicesHostBuilderExtension.cs
+++ INStore/HostBuilders/AddServicesHostBuilderExtension.cs
@@ -1,7 +1,9 @@
 ﻿using INStore.Domain.Services;
 using INStore.Domain.Services.AuthenticationService;
 using INStore.EntityFramework.Services;
+using INStore.Language;
 using INStore.State.Authenticators;
+using INStore.State.Registers;
 using INStore.State.UserStore;
 using MaterialDesignThemes.Wpf;
 using Microsoft.AspNet.Identity;
@@ -23,6 +25,7 @@ namespace INStore.HostBuilders
                 services.AddSingleton<IAuthenticationService, AuthenticationService>();
                 services.AddSingleton<IAuthenticators, Authenticators>();
                 services.AddSingleton<ISnackbarMessageQueue, SnackbarMessageQueue>();
+                services.AddSingleton<IRegister, Register>();
             });
             return host;
         }
