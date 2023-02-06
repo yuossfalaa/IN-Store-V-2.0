6adb5cb908a4d5e524e76e23f19a6d43bf0e03fb
yuossfalaa <yuossfalla@gmail.com>
2023-02-06 11:26:49 +0200

    .

---
+++ INStore/UserControls/SignUp_IN/Commands/RegisterCommand.cs
@@ -0,0 +1,38 @@
+﻿using INStore.Commands;
+using INStore.State.Authenticators;
+using INStore.State.Navigators;
+using INStore.State.UserStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static INStore.Domain.Services.AuthenticationService.IAuthenticationService;
+
+namespace INStore.UserControls.SignUp_IN.Commands
+{
+    public class RegisterCommand : AsyncCommandBase
+    {
+        private readonly IAuthenticators authenticators;
+        private readonly IInRegistrationUser inRegistrationUser;
+        private readonly IRenavigator renavigator;
+
+        public RegisterCommand(IAuthenticators authenticators, IInRegistrationUser inRegistrationUser, IRenavigator renavigator)
+        {
+            this.authenticators = authenticators;
+            this.inRegistrationUser = inRegistrationUser;
+            this.renavigator = renavigator;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            RegistrationResult result = RegistrationResult.Success;
+
+            result = await authenticators.Register(inRegistrationUser);
+            if (result == RegistrationResult.Success)
+            {
+                renavigator.Renavigate();
+            }
+        }
+    }
+}
