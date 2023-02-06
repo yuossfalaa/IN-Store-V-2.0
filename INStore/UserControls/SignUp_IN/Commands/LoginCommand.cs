59a97499138caaa8fd6d9bb7ae6e7c9ae55841dd
yuossfalaa <yuossfalla@gmail.com>
2023-02-06 14:46:05 +0200

    Adding Logging

--- INStore/UserControls/SignUp_IN/Commands/LoginCommand.cs
+++ INStore/UserControls/SignUp_IN/Commands/LoginCommand.cs
@@ -3,6 +3,9 @@ using INStore.Domain.Exceptions;
 using INStore.State.Authenticators;
 using INStore.State.Navigators;
 using INStore.UserControls.SignUp_IN.ViewModels;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace INStore.UserControls.SignUp_IN.Commands
@@ -12,24 +15,41 @@ namespace INStore.UserControls.SignUp_IN.Commands
         private readonly LoginViewModel _loginViewModel;
         private readonly IAuthenticators _authenticators;
         private readonly IRenavigator _renavigator;
+        private readonly ISnackbarMessageQueue snackbarMessageQueue;
+        private readonly ILogger<LoginViewModel> _Logger;
 
-        public LoginCommand(LoginViewModel loginViewModel, IAuthenticators authenticators, IRenavigator Renavigator)
+
+        public LoginCommand(LoginViewModel loginViewModel, IAuthenticators authenticators, IRenavigator Renavigator, ISnackbarMessageQueue snackbarMessageQueue, ILogger<LoginViewModel> logger)
         {
             _authenticators = authenticators;
             _renavigator = Renavigator;
             _loginViewModel = loginViewModel;
+            this.snackbarMessageQueue = snackbarMessageQueue;
+            _Logger = logger;
         }
         public override async Task ExecuteAsync(object parameter)
         {
             try
             {
                 await _authenticators.Login(_loginViewModel.Username, _loginViewModel.Password);
-
+                _Logger.LogInformation("successful login");
                 _renavigator.Renavigate();
             }
-            catch (UserNotFoundException)
-            {
+            catch (UserNotFoundException userNotFound)
+            { 
+                snackbarMessageQueue.Enqueue("The User : " + userNotFound.Username +" Not Found");
+                _Logger.LogTrace("The User : " + userNotFound.Username + " Not Found");
 
+            }
+            catch (WrongPasswordExcption wrongPassword)
+            {
+                snackbarMessageQueue.Enqueue("Wrong Password");
+                _Logger.LogTrace("Wrong Password");
+
+            }
+            catch (Exception exception)
+            {
+                _Logger.LogError(exception.Message);
             }
         }
     }
