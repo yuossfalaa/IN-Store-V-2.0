7db6d3de6f79f01bc3e60a23b707e3f93033465f
yuossfalaa <yuossfalla@gmail.com>
2023-02-15 20:53:46 +0200

    Adding MyStore ViewModel

---
+++ INStore/UserControls/MyStore/ViewModels/MyStoreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INStore.ViewModels;
+
+namespace INStore.UserControls.MyStore.ViewModels
+{
+    public class MyStoreViewModel : ViewModelBase
+    {
+    }
+}
