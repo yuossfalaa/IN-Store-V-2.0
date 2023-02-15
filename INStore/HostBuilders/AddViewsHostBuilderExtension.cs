19aa652d2d92186922a312a6b8500f51b361c478
yuossfalaa <yuossfalla@gmail.com>
2023-02-15 20:28:38 +0200

    Logout Func

--- INStore/HostBuilders/AddViewsHostBuilderExtension.cs
+++ INStore/HostBuilders/AddViewsHostBuilderExtension.cs
@@ -1,6 +1,14 @@
-﻿using INStore.ViewModels;
+﻿using INStore.Factories;
+using INStore.Language;
+using INStore.State.Authenticators;
+using INStore.State.Navigators;
+using INStore.UserControls.SignUp_IN.ViewModels;
+using INStore.ViewModels;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace INStore.HostBuilders
 {
@@ -10,10 +18,24 @@ namespace INStore.HostBuilders
         {
             host.ConfigureServices(services =>
             {
-                services.AddSingleton<MainWindow>(s => new MainWindow(s.GetRequiredService<MainViewModel>()));
+                services.AddSingleton<MainWindow>(s => new MainWindow(CreatMainViewModel(s)));
             });
 
             return host;
+        }
+
+        private static object CreatMainViewModel(IServiceProvider s)
+        {
+            return new MainViewModel
+                (
+                    s.GetRequiredService< ILogger <MainViewModel>>(),
+                    s.GetRequiredService<INavigator>(),
+                    s.GetRequiredService<IINStoreViewModelFactory>(),
+                    s.GetRequiredService<ISnackbarMessageQueue>(),
+                    s.GetRequiredService<ILanguageSetter>(),
+                    s.GetRequiredService<IAuthenticators>(),
+                    s.GetRequiredService<ViewModelDelegateRenavigator<LoginViewModel>>()
+                );
         }
     }
 }
