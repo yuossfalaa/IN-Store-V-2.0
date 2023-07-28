7b03a5553b68f197962be3478c44138fcb70f2d9
yuossfalaa <yuossfalla@gmail.com>
2023-07-28 06:31:47 +0300

    .

--- INStore/UserControls/Home/ViewModels/HomeViewModel.cs
+++ INStore/UserControls/Home/ViewModels/HomeViewModel.cs
@@ -1,8 +1,25 @@
-﻿using INStore.ViewModels;
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using INStore.State.FloatingWindow;
+using INStore.UserControls.MyStore.ViewModels;
+using INStore.ViewModels;
 
 namespace INStore.UserControls.Home.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        public FloatingWindow FloatingWindowControl;
+        public ICommand OpenFloatingWindow {  get; set; }
+        public HomeViewModel(FloatingWindow floatingWindowControl)
+        {
+            FloatingWindowControl = floatingWindowControl;
+            OpenFloatingWindow = new RelayCommand(OpenFloatingWindowFunc);
+
+        }
+
+        private void OpenFloatingWindowFunc()
+        {
+            MaterialDesignThemes.Wpf.DialogHost.OpenDialogCommand.Execute(null, null);
+        }
     }
 }
