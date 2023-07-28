c5c6415a5c02d976d49fbf370da61dda19637d5f
yuossfalaa <yuossfalla@gmail.com>
2023-07-28 06:32:32 +0300

    .

---
+++ INStore.EntityFramework/Migrations/20230726042254_ImageForitems.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INStore.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageForitems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "StoreItemProperties",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "StoreItemProperties");
+        }
+    }
+}
