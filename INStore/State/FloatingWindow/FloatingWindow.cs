a72c65158e1013941131690ea2e181bbb793d898
yuossfalaa <yuossfalla@gmail.com>
2023-07-28 06:31:40 +0300

    .

---
+++ INStore/State/FloatingWindow/FloatingWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using INStore.ViewModels;
+
+namespace INStore.State.FloatingWindow
+{
+    public class FloatingWindow 
+    {
+        private ViewModelBase _FloatingWindowControl;
+
+        public ViewModelBase FloatingWindowControl
+        {
+            get { return _FloatingWindowControl; }
+            set { _FloatingWindowControl = value; StateChanged?.Invoke(); }
+        }
+        public event Action StateChanged;
+    }
+}
