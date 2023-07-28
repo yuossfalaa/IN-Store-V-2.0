75158f950d4bb4ee3e632acb7ad1e47aa6e2a7cc
yuossfalaa <yuossfalla@gmail.com>
2023-07-28 06:32:47 +0300

    .

--- INStore.Domain/Models/StoreItems.cs
+++ INStore.Domain/Models/StoreItems.cs
@@ -3,7 +3,7 @@
     public class StoreItems : DomainObject
     {
         public int StoreItemPropertiesId { get; set; }  
-        public StoreItemProperties Item { get; set; }
+        public StoreItemProperties Item { get; set; } = new StoreItemProperties();
         public int ItemNumberInStore { get; set; } = 0;
         public int ItemNumberInStock { get; set; }=0;
        
