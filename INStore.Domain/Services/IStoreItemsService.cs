98ff0550e8411555b80e8de4185952f830e8ec00
yuossfalaa <yuossfalla@gmail.com>
2023-07-28 06:32:37 +0300

    .

---
+++ INStore.Domain/Services/IStoreItemsService.cs
@@ -0,0 +1,9 @@
+﻿using INStore.Domain.Models;
+
+namespace INStore.Domain.Services
+{
+    public interface IStoreItemsService : IDataService<StoreItems>
+    {
+        public Task<List<StoreItems>> Get(string StoreItemProp);
+    }
+}
