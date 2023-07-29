f126f6bd12e59f46c27adc20db2e413950f38380
yuossfalaa <yuossfalla@gmail.com>
2023-07-29 15:34:46 +0300

    .

--- INStore/UserControls/MyStore/Commands/EditStoreItem.cs
+++ INStore/UserControls/MyStore/Commands/EditStoreItem.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using INStore.Commands;
+using INStore.Domain.Models;
 using INStore.Domain.Services;
+using INStore.Language;
 using INStore.UserControls.MyStore.ViewModels;
 using Microsoft.Extensions.Logging;
 
@@ -21,6 +25,11 @@ namespace INStore.UserControls.MyStore.Commands
 
         public override async Task ExecuteAsync(object parameter)
         {
+            if (await TheSameBarcodeExist())
+            {
+                _myStoreViewModel._SnackbarMessageQueue.Enqueue(LocalizedStrings.Instance["AddStoreItemTheSameBarcodeExist"]);
+                return;
+            }
             try
             {
                 await _storeItemsService.Update(_editItemViewModel.StoreItem.Id, _editItemViewModel.StoreItem);
@@ -32,6 +41,15 @@ namespace INStore.UserControls.MyStore.Commands
             {
                 _myStoreViewModel._MyStoreViewModelLogger.LogError(ex.Message);
             }
+        }
+        private async Task<bool> TheSameBarcodeExist()
+        {
+            List<StoreItems> HavethesameBarcode = await _storeItemsService.Get(_editItemViewModel.StoreItem.Item.ItemBarCode);
+            if (HavethesameBarcode.Where(a => a.Id != _editItemViewModel.StoreItem.Id && a.Item.ItemBarCode == _editItemViewModel.StoreItem.Item.ItemBarCode).Count() >= 1)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
