e97d7704e7acc6c1c6e21b50fa211231052b6983
yuossfalaa <yuossfalla@gmail.com>
2023-08-05 18:59:13 +0300

    .

--- INStore.Domain/Models/SellingHistory.cs
+++ INStore.Domain/Models/SellingHistory.cs
@@ -5,8 +5,35 @@
         public int ReceiptID { get; set; }
         public Receipts Receipt { get; set; }
         public string ItemName { get; set; } = "";
-        public int ItemNumber { get; set; } = 0;
-        public double ItemSellingPrice { get; set; } = 0.0;
-        public double ItemTotalCost { get; set; } =  0.0;
+
+        private int _ItemNumber=0;
+
+        public int ItemNumber
+        {
+            get { return _ItemNumber; }
+            set { _ItemNumber = value; OnPropertyChanged(nameof(ItemNumber)); }
+        }
+
+
+
+        private double _ItemSellingPrice=0.0;
+
+        public double ItemSellingPrice
+        {
+            get { return _ItemSellingPrice; }
+            set { _ItemSellingPrice = value; OnPropertyChanged(nameof(ItemSellingPrice)); }
+        }
+
+
+
+        private double _ItemTotalCost = 0.0;
+
+        public double ItemTotalCost
+        {
+            get { return _ItemTotalCost; }
+            set { _ItemTotalCost = value; OnPropertyChanged(nameof(ItemTotalCost)); }
+        } 
+
+
     }
 }
