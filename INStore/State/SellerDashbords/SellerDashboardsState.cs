666051e3b6c610ebfb8881aeaaae44479d2839f8
yuossfalaa <yuossfalla@gmail.com>
2023-08-05 18:58:08 +0300

    .

---
+++ INStore/State/SellerDashbords/SellerDashboardsState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using INStore.UserControls.Home.ViewModels;
+using INStore.ViewModels;
+
+namespace INStore.State.SellerDashbords
+{
+    public class SellerDashboardsState : ViewModelBase
+    {
+        private ObservableCollection<SellerDashboard> _sellerDashboards;
+
+        public ObservableCollection<SellerDashboard> SellerDashboards
+        {
+            get { return _sellerDashboards; }
+            set { _sellerDashboards = value; OnPropertyChanged(nameof(SellerDashboards)); }
+        }
+        public SellerDashboardsState()
+        {
+            SellerDashboards = new ObservableCollection<SellerDashboard> { new SellerDashboard { DashboardName = "Dashboard 1" } };
+        }
+    }
+}
