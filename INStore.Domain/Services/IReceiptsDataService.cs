41ce79ef71fb9b0f62bae651d559509a6256c314
yuossfalaa <yuossfalla@gmail.com>
2023-08-06 14:35:48 +0300

    .

---
+++ INStore.Domain/Services/IReceiptsDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INStore.Domain.Models;
+
+namespace INStore.Domain.Services
+{
+    public interface IReceiptsDataService : IDataService<Receipts>
+    {
+    }
+}
