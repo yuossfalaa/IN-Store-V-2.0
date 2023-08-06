4ecd00aca5ca3a2a55c3f60fc40f0fadf19fe1f8
yuossfalaa <yuossfalla@gmail.com>
2023-08-06 14:35:31 +0300

    .

---
+++ INStore/Services/ReceiptsServices/IReceiptService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using INStore.Domain.Models;
+
+namespace INStore.Services.ReceiptsServices
+{
+    public interface IReceiptService
+    {
+        Task PayReceipt(Receipts receipts);
+        Task RefundReceipt(Receipts receipts);
+        Task UpdateReceipt(Receipts receipts);
+    }
+}
