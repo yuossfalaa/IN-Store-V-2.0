46b64fea52b88d2a58cf8e38562c283a9cf4ac85
yuossfalaa <yuossfalla@gmail.com>
2023-08-07 06:18:58 +0300

    .

---
+++ INStore/UserControls/Orders/ViewModel/OrdersViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using INStore.Commands;
+using INStore.Domain.Models;
+using INStore.Domain.Services;
+using INStore.Services.StoreItemServices;
+using INStore.State.Navigators;
+using INStore.UserControls.Home.ViewModels;
+using INStore.ViewModels;
+
+namespace INStore.UserControls.Orders.ViewModel
+{
+    public class OrdersViewModel : ViewModelBase
+    {
+        #region Private Vars
+        private readonly IRenavigator homeRenavigator;
+        #endregion
+        #region Commands
+        public ICommand HomeNavigationCommand { get; set; }
+        #endregion
+        public OrdersViewModel(ViewModelDelegateRenavigator<HomeViewModel> homeRenavigator)
+        {
+            this.homeRenavigator = homeRenavigator;
+
+            #region Init Command
+            HomeNavigationCommand = new RenavigateCommand(this.homeRenavigator);
+            #endregion
+
+        }
+    }
+}
