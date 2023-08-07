38f29cb781c7b86d447eb80305ee5da8c3f33a5b
yuossfalaa <yuossfalla@gmail.com>
2023-08-07 08:19:26 +0300

    adding barcode

--- INStore.Domain/Services/IStoreItemsService.cs
+++ INStore.Domain/Services/IStoreItemsService.cs
@@ -4,6 +4,11 @@ namespace INStore.Domain.Services
 {
     public interface IStoreItemsService : IDataService<StoreItems>
     {
+        /// <summary>
+        /// Get a Store Item Based on the Name or the Barcode
+        /// </summary>
+        /// <param name="StoreItemProp"></param>
+        /// <returns>Store Items List with all Matching Items</returns>
         public Task<List<StoreItems>> Get(string StoreItemProp);
     }
 }
