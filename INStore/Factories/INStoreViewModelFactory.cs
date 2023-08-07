c084ccee8947ff967d3caeb6fe838f9288776a1a
yuossfalaa <yuossfalla@gmail.com>
2023-08-07 06:18:15 +0300

    .

--- INStore/Factories/INStoreViewModelFactory.cs
+++ INStore/Factories/INStoreViewModelFactory.cs
@@ -1,5 +1,6 @@
 ﻿using INStore.UserControls.Home.ViewModels;
 using INStore.UserControls.MyStore.ViewModels;
+using INStore.UserControls.Orders.ViewModel;
 using INStore.UserControls.SignUp_IN.ViewModels;
 using INStore.ViewModels;
 using System;
@@ -14,12 +15,14 @@ namespace INStore.Factories
         private readonly CreateViewModel<RegisterViewModel> _createRegisterViewModel;
         private readonly CreateViewModel<RegisterEmployeeViewModel> _createRegisterEmployeeViewModel;
         private readonly CreateViewModel<MyStoreViewModel> _createMyStoreViewModel;
+        private readonly CreateViewModel<OrdersViewModel> _createOrdersViewModel;
 
         public INStoreViewModelFactory(CreateViewModel<HomeViewModel> createHomeViewModel,
             CreateViewModel<LoginViewModel> createLoginViewModel,
             CreateViewModel<RegisterViewModel> createRegisterViewModel,
             CreateViewModel<RegisterEmployeeViewModel> createRegisterEmployeeViewModel,
-            CreateViewModel<MyStoreViewModel> createMyStoreViewModel)
+            CreateViewModel<MyStoreViewModel> createMyStoreViewModel,
+            CreateViewModel<OrdersViewModel> createOrdersViewModel)
 
         {
             _createHomeViewModel = createHomeViewModel;
@@ -27,6 +30,7 @@ namespace INStore.Factories
             _createRegisterViewModel = createRegisterViewModel;
             _createRegisterEmployeeViewModel = createRegisterEmployeeViewModel;
             _createMyStoreViewModel = createMyStoreViewModel;
+            _createOrdersViewModel = createOrdersViewModel;
         }
         public ViewModelBase CreateViewModel(ViewType viewType)
         {
@@ -46,6 +50,9 @@ namespace INStore.Factories
                     break;
                 case ViewType.MyStore:
                     return _createMyStoreViewModel();
+                    break; 
+                case ViewType.Orders:
+                    return _createOrdersViewModel();
                     break;
                 default:
                     throw new ArgumentException("The ViewType does not have a ViewModel.", "viewType");
