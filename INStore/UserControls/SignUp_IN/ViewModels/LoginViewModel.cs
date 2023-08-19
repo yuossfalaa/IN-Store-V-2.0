3c216800ce5671e53c38122cab1dbdcda6e5d81e
yuossfalaa <yuossfalla@gmail.com>
2023-08-19 23:36:09 +0300

    `

--- INStore/UserControls/SignUp_IN/ViewModels/LoginViewModel.cs
+++ INStore/UserControls/SignUp_IN/ViewModels/LoginViewModel.cs
@@ -1,38 +1,37 @@
-﻿using INStore.Commands;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using INStore.Commands;
+using INStore.Domain.Exceptions;
 using INStore.Domain.Models;
+using INStore.Language;
 using INStore.State.Authenticators;
 using INStore.State.Navigators;
 using INStore.State.UserStore;
 using INStore.UserControls.Home.ViewModels;
-using INStore.UserControls.SignUp_IN.Commands;
 using INStore.ViewModels;
 using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace INStore.UserControls.SignUp_IN.ViewModels
 {
-    public class LoginViewModel : ViewModelBase
+    public partial class LoginViewModel : ViewModelBase
     {
+        #region Private Vars
         private readonly ILogger<LoginViewModel> _LoginViewModelLogger;
         private readonly IAuthenticators _Authenticators;
         private readonly IRenavigator _loginRenavigator;
         private readonly IRenavigator _registerRenavigator;
         private readonly ISnackbarMessageQueue snackbarMessageQueue;
         private readonly IInRegistrationUser _inRegistrationUser;
-        private User _currentUser;
+        #endregion
 
-        public User CurrentUser
-        {
-            get { return _currentUser; }
-            set
-            {
-                _currentUser = value;
-                OnPropertyChanged(nameof(CurrentUser));
-            }
-        }
-        public ICommand LoginCommand { get; set; }
-        public ICommand RegisterCommand { get; set; }
+        [ObservableProperty]
+        User currentUser;
+        
+         public ICommand RegisterCommand { get; set; }
 
         public LoginViewModel(ILogger<LoginViewModel> loginViewModelLogger, IAuthenticators authenticators,
             ViewModelDelegateRenavigator<HomeViewModel> loginRenavigator, ViewModelDelegateRenavigator<RegisterViewModel> registerRenavigator,
@@ -47,8 +46,7 @@ namespace INStore.UserControls.SignUp_IN.ViewModels
 
 
             CurrentUser = new User();
-            LoginCommand = new LoginCommand(this, authenticators, loginRenavigator, snackbarMessageQueue, _LoginViewModelLogger);
-            RegisterCommand = new RenavigateCommand(registerRenavigator);
+             RegisterCommand = new RenavigateCommand(registerRenavigator);
             _inRegistrationUser.RegisteringUser = new User();
 
             _LoginViewModelLogger.Log(LogLevel.Information, "LoginViewModel Initialized");
@@ -59,14 +57,39 @@ namespace INStore.UserControls.SignUp_IN.ViewModels
 #endif
         }
 
-
-        public override void Dispose()
+        #region Commands
+        [RelayCommand]
+        public async Task Login()
         {
-            base.Dispose();
+            
+            try
+            {
+                await Task.Run(async () => 
+                {
+                    await _Authenticators.Login(CurrentUser.UserName, CurrentUser.PasswordHash);
+                });
+                _LoginViewModelLogger.LogInformation("successful login");
+                snackbarMessageQueue.Enqueue(LocalizedStrings.Instance["LoginCommandWelcomeBack"] + _Authenticators.CurrentUser.Employee.EmployeeName);
+                _loginRenavigator.Renavigate();
+            }
+            catch (UserNotFoundException userNotFound)
+            {
+                snackbarMessageQueue.Enqueue(LocalizedStrings.Instance["LoginCommandTheUser"] + userNotFound.Username + LocalizedStrings.Instance["LoginCommandNotFound"]);
+                _LoginViewModelLogger.LogTrace("The User : " + userNotFound.Username + " Not Found");
+
+            }
+            catch (WrongPasswordExcption wrongPassword)
+            {
+                snackbarMessageQueue.Enqueue(LocalizedStrings.Instance["LoginCommandWrongPassword"]);
+                _LoginViewModelLogger.LogTrace("Wrong Password");
+
+            }
+            catch (Exception exception)
+            {
+                _LoginViewModelLogger.LogError(exception.Message);
+            }
         }
-
-
-
+        #endregion
 
     }
 }
